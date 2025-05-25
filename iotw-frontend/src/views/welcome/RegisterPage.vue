816fda3507e6cd04532ab583a4e9c833505cd1f2
HuangWS <3070358878@qq.com>
2025-05-25 15:12:17 +0800

    注册界面规则

--- iotw-frontend/src/views/welcome/RegisterPage.vue
+++ iotw-frontend/src/views/welcome/RegisterPage.vue
@@ -14,7 +14,48 @@ const form = reactive({
   code: ''
 })
 
+const validateUsername = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error("请输入用户名"));
+  } else if (!/^[a-zA-Z0-9\u4e00-\u9fa5]+$/.test(value)) {
+    callback(new Error("用户名不能包含特殊字符, 只能是中/英文"));
+  } else {
+    callback();
+  }
+
+}
+const validatePassword = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请再次输入密码'))
+  } else if (value !== form.password) {
+    callback(new Error("两次输入的密码不一致"))
+  } else {
+    callback()
+  }
+}
+
+
 const rule = {
+  username: [
+    {validator: validateUsername, trigger: ['blur', 'change']},
+    {min: 2, max: 10, message: '用户名长度必须在2-8个字符之间', trigger: ['blur', 'change']},
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'},
+    {min: 6, max: 16, message: '密码的长度必须在6-16个字符之间', trigger: ['blur', 'change']}
+  ],
+  passwordConfirm: [
+    {validator: validatePassword, trigger: ['blur', 'change']},
+  ],
+  email: [
+    {required: true, message: '请输入邮件地址', trigger: 'blur'},
+    {type: 'email', message: '请输入合法的电子邮件地址', trigger: ['blur', 'change']}
+  ],
+  code: [
+    {required: true, message: '请输入获取的验证码', trigger: 'blur'},
+  ]
+}
+
 const coldTime = ref(0)
 
 function askCode() {
@@ -62,24 +103,30 @@ const isEmailValid = computed(() => /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9
     </div>
     <div style="margin-top: 50px">
       <el-form :model="form" :rules="rule">
-        <el-form-item>
-          <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名">
+        <el-form-item prop="username">
+          <el-input v-model="form.username" maxlength="15" type="text" placeholder="用户名">
             <template #prefix>
-              <el-icon><User /></el-icon>
+              <el-icon>
+                <User/>
+              </el-icon>
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input v-model="form.password" maxlength="20" type="password" placeholder="密码">
             <template #prefix>
-              <el-icon><Lock /></el-icon>
+              <el-icon>
+                <Lock/>
+              </el-icon>
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="passwordConfirm">
           <el-input v-model="form.passwordConfirm" maxlength="20" type="password" placeholder="确认密码">
             <template #prefix>
-              <el-icon><Lock /></el-icon>
+              <el-icon>
+                <Lock/>
+              </el-icon>
             </template>
           </el-input>
         </el-form-item>
