a1e6fdad0861f1c35180a3c8d3bb8646c96be4b8
HuangWS <3070358878@qq.com>
2025-05-25 12:35:03 +0800

    注册界面初始化

---
+++ iotw-frontend/src/views/welcome/RegisterPage.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+
+import {reactive} from "vue";
+import {Lock, User, Message, Edit} from "@element-plus/icons-vue";
+import router from "@/router";
+
+const  form = reactive({
+  username: '',
+  password: '',
+  passwordConfirm: '',
+  email: '',
+  code: ''
+})
+
+const rule = {
+
+}
+
+</script>
+
+<template>
+  <div style="text-align: center;margin: 0 20px">
+    <div style="margin-top: 100px">
+      <div style="font-size: 25px;font-weight: bold">注册新用户</div>
+      <div style="font-size: 14px;color: grey">欢迎注册我们的学习平台，请在下方填写相关信息</div>
+    </div>
+    <div style="margin-top: 50px">
+      <el-form :model="form" :rules="rule">
+        <el-form-item>
+          <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名">
+            <template #prefix>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.password" maxlength="20" type="password" placeholder="密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.passwordConfirm" maxlength="20" type="password" placeholder="确认密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.email" minlength="4" type="text" placeholder="邮箱">
+            <template #prefix>
+              <el-icon><Message /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-row gutter="10" style="width: 100%">
+            <el-col :span="17">
+              <el-input v-model="form.code" maxlength="6" type="text" placeholder="请输入验证码">
+                <template #prefix>
+                  <el-icon><Edit /></el-icon>
+                </template>
+              </el-input>
+            </el-col>
+            <el-col :span="5">
+              <el-button type="success" plain>获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+      <div style="margin-top: 80px">
+        <el-button style="width: 270px" type="primary" plain>立即注册</el-button>
+      </div>
+      <div style="margin-top: 20px">
+        <span style="font-size: 14px;line-height: 15px;color: grey">已有账号？</span>
+        <el-link style="translate: 0 -2px" @click="router.push('/')">立即登录</el-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
