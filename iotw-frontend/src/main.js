dc37ae6061e5f4e8acff7f1da143928d998ed4bb
HuangWS <3070358878@qq.com>
2025-05-26 15:42:43 +0800

    pinia初始化

--- iotw-frontend/src/main.js
+++ iotw-frontend/src/main.js
@@ -1,14 +1,17 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
 import axios from "axios";
+import {createPinia} from "pinia";
 
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
 axios.defaults.baseURL = "http://localhost:8080";
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
