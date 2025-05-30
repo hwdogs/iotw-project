d6bdd32a1c4a0d8d050c468e9e546685fb11347e
HuangWS <3070358878@qq.com>
2025-05-30 15:24:13 +0800

    优化jwt

--- iotw-backend/src/main/java/org/example/utils/JwtUtils.java
+++ iotw-backend/src/main/java/org/example/utils/JwtUtils.java
@@ -125,7 +125,7 @@ public class JwtUtils {
         return JWT.create()
                 .withJWTId(UUID.randomUUID().toString())    //每个令牌都有自己随机的uuid
                 .withClaim("id", id)
-                .withClaim("username", username)
+                .withClaim("name", username)
                 .withClaim("authorities", details.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
                 .withExpiresAt(expire)
                 .withIssuedAt(new Date())
@@ -147,7 +147,7 @@ public class JwtUtils {
     public UserDetails toUser(DecodedJWT jwt) {
         Map<String, Claim> claims = jwt.getClaims();
         return User
-                .withUsername(claims.get("username").asString())
+                .withUsername(claims.get("name").asString())
                 .password("*******")
                 .authorities(claims.get("authorities").asArray(String.class))
                 .build();
