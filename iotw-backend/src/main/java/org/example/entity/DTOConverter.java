a6c05cc10edb42fd60b225212eefc61b5246238f
HuangWS <3070358878@qq.com>
2025-06-04 17:53:16 +0800

    优化对父类属性的转换

--- iotw-backend/src/main/java/org/example/entity/DTOConverter.java
+++ iotw-backend/src/main/java/org/example/entity/DTOConverter.java
@@ -7,15 +7,20 @@ import java.util.function.Consumer;
  * 可以快速转化为指定的vo对象
  *
  * @author hwshou
- * @date 2025/5/29  14:36
+ * @date 2025/5/29 14:36
  */
 public interface DTOConverter {
     default <D> D asDTO(Class<D> dtoClass, Consumer<D> postProcessor) {
         try {
             D dto = dtoClass.getDeclaredConstructor().newInstance();
             // 关键点：遍历VO字段复制到DTO
-            for (Field voField : this.getClass().getDeclaredFields()) {
-                copyField(voField, dto, dtoClass);
+            // 递归复制当前类及所有父类的字段
+            Class<?> currentClass = this.getClass();
+            while (currentClass != null && currentClass != Object.class) {
+                for (Field voField : currentClass.getDeclaredFields()) {
+                    copyField(voField, dto, dtoClass);
+                }
+                currentClass = currentClass.getSuperclass(); // 向上遍历父类
             }
             postProcessor.accept(dto);
             return dto;
