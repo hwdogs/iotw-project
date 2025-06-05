248b713585f8a48798a138ffafec67a192050834
HuangWS <3070358878@qq.com>
2025-06-06 00:13:59 +0800

    完成供应商服务层接口

--- iotw-backend/src/main/java/org/example/service/SupplierService.java
+++ iotw-backend/src/main/java/org/example/service/SupplierService.java
@@ -1,17 +1,23 @@
 package org.example.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.example.entity.dto.Supplier;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.example.entity.vo.request.*;
+import org.example.entity.vo.response.SupplierTableVO;
 
 /**
  * <p>
- *  服务类
+ * supplier服务类
  * </p>
  *
  * @author hwshou
  * @since 2025-05-28 20:16
  */
 public interface SupplierService extends IService<Supplier> {
+    IPage<SupplierTableVO> querySupplierTableByCondition(SupplierQueryVO vo);
+
+    String logicDeleteOneSupplier(Integer id);
 
     String registerOneSupplier(AccountEmailRegisterVO vo);
 
